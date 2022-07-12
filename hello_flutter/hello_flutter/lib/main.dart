ddc07e901f8ab1ebce4d9f49cd2572984f30d2af
Erin Clark <erincodepirate@gmail.com>
2022-07-11 17:47:05 -0700

    Add basic hello world flutter app

---
+++ hello_flutter/hello_flutter/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Hello Flutter",
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
