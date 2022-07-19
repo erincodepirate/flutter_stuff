78008b9a7d1559ad8765fcb96cd7205f2c4586bf
Erin Clark <erincodepirate@gmail.com>
2022-07-18 17:43:07 -0700

    Move code around

--- hello_flutter/hello_flutter/lib/main.dart
+++ hello_flutter/hello_flutter/lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/ui/home.dart';
 
 void main() {
   runApp(const Home());
-}
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-        color: Colors.orange,
-        child: Center(
-            child: Text("Hello Flutter",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 23.4,
-                    fontStyle: FontStyle.italic))));
-  }
 }
---
+++ hello_flutter/hello_flutter/lib/ui/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.orange,
+        child: Center(
+            child: Text("Hello Flutter",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 23.4,
+                    fontStyle: FontStyle.italic))));
+  }
+}
